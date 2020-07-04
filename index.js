8e0b6205611175c5a52c273dd2e50866cef9de00
BlueSyntaxyt <67604883+BlueSyntaxyt@users.noreply.github.com>
2020-07-04 11:28:27 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+exports.Check = function (first, second, bool) {
+    if (!first) throw new Error("Missing first argument");
+    if (!second) throw new Error("Missing second argument");
+    if (!bool) throw new Error("Missing bool argument");
+    if (!typeof bool === "boolean") throw new Error("Third argument is not a boolean");
+    if (first == second) {
+        bool = true;
+    } else {
+        bool = false;
+    }
+}
+exports.Calc = function (first, second, way, third) {
+    if (!first) throw new Error("Missing first argument");
+    if (!second) throw new Error("Missing second argument");
+    if (!way) throw new Error("Missing instructions on how to calculate. (third argument)");
+    if (!third) throw new Error("Missing fourth argument");
+    if (!typeof first === "float") {
+        if (!typeof first === "int") {
+            throw new Error("First argument is not a float or int");
+        }
+    }
+    if (!typeof second === "float") {
+        if (!typeof second === "int") {
+            throw new Error("Second argument is not a float or int");
+        }
+    }
+    if (!typeof way === "string") throw new Error("Third argument is not a string");
+    if (!typeof third === "boolean") throw new Error("Fourth argument is not a boolean");
+    if (way == "==") {
+        if (first == second) {
+            third = true;
+        } else {
+            third = false;
+        }
+    } else {
+        if (way == "<=") {
+            if (first <= second) {
+                third = true;
+            } else {
+                third = false;
+            }
+        } else {
+            if (way == ">=") {
+                if (first >= second) {
+                    third = true;
+                } else {
+                    third = false;
+                }
+            } else {
+                if (way == "===") {
+                    if (first === second) {
+                        third = true;
+                    } else {
+                        third = false;
+                    }
+                } else {
+                    throw new Error("Third argument is not a valid calculation identifier");
+                }
+            }
+        }
+    }
+}
